9d42246d75be06e149ca02099a079f60a932c7ef
markjohnacaling <87033398+markjohnacaling@users.noreply.github.com>
2021-07-07 05:29:49 +0800

    Update index.php

--- Acaling_MarkJohnXavier/Exam_01/index.php
+++ Acaling_MarkJohnXavier/Exam_01/index.php
@@ -1,1 +1,21 @@
-
+<?php
+// PHP code to check for Palindrome string in PHP
+// Using strrev()
+function Palindrome($string){ 
+    if (strrev($string) == $string){ 
+        return 1; 
+    }
+    else{
+        return 0;
+    }
+} 
+ 
+// Driver Code
+$original = "DAD";
+if(Palindrome($original)){ 
+    echo "Palindrome"; 
+}
+else { 
+echo "Not a Palindrome"; 
+}
+?> 
