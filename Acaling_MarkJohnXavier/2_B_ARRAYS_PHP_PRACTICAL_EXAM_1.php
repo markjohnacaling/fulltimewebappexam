6a50add872f61a17a1ced0f1dd89c4593b3cd6c8
markjohnacaling <87033398+markjohnacaling@users.noreply.github.com>
2021-07-07 05:03:05 +0800

    Create 2_B_ARRAYS_PHP_PRACTICAL_EXAM_1.php

---
+++ Acaling_MarkJohnXavier/2_B_ARRAYS_PHP_PRACTICAL_EXAM_1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body>
+
+<?php
+$numbers = array(9863, 7127, 2020, 80,131, 55, 100);
+sort($numbers);
+	$length = count($numbers);
+ for($x = 0; $x < $length; $x++)
+ {
+if($names[$x] % 2 != 0){
+$numbers[$x] = round($numbers[$x], 2) ;
+ }
+ echo $numbers[$x] . "<br>";
+
+}
+
+
+?>
+</body>
+</html>
